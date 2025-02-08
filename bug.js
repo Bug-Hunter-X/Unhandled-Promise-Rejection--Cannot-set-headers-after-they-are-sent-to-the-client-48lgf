97dea17c85a3ee7d1df17970195a08bfe2d9674f
BugHunter-X <askzenai@gmail.com>
2025-02-08 18:02:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello from Node.js!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
+//Error: Unhandled promise rejection
+//Reason: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
